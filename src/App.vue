b1e3e8461424577c1e6bd7731c6cb92605470d95
Jake O'Rielly <jake.orielly@gmail.com>
2021-03-08 03:35:45 +0000

    Setting up

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ReccomenderWindow />
+  </div>
+</template>
+
+<script>
+import ReccomenderWindow from './components/ReccomenderWindow.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ReccomenderWindow
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ReccomenderWindow.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>
+      Hi
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ReccomenderWindow',
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
